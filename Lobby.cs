a7e314206af796b70df4818c871f5b1fb56381bb
george521 <37348405+george521@users.noreply.github.com>
2023-02-11 21:54:31 +0200

    Lan Tic Tac Toe App with simple UI

---
+++ Lobby.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Lobby : Form
+    {
+        public Lobby()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Game newGame =  new Game(false,textBox1.Text);
+            Visible = false;
+            if(!newGame.IsDisposed)
+                newGame.ShowDialog();
+            Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Game newGame = new Game(true);
+            Visible = false;
+            if (!newGame.IsDisposed)
+                newGame.ShowDialog();
+            Visible = true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+
+namespace Tic_Tac_Toe
+{
+    public static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Lobby());
+        }
+    }
+
+}
